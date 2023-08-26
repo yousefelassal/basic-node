434c9e1f99ce370016881dd0735e9cb875fb9802
Yousef <yousefsameh24@gmail.com>
2023-08-26 20:57:27 +0300

    test morgan

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const logger = require('morgan');
 const app = express();
 const port = 3000;
 
+app.use(logger('dev'))
+
 app.get('/', (req, res) => {
   res.send('Hello World!!');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Hello Test!!');
 });
 
 app.listen(port, () => {
